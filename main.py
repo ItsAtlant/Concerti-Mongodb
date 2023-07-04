d87429e5143484c69b01d44c4d72f5d8c043d752
ItsAtlant <Davide02072003@gmail.com>
2023-07-04 18:28:19 +0200

    Geolocalizzazione e login fatti
    geolocalizzazione implementata, login implementato, visualizzazione dei
    biglietti e dei concerti effettutato

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 from bson import ObjectId
+from bson import DatetimeConversion
 from pymongo import MongoClient
+from geopy.geocoders import Nominatim
+import datetime
 
+
+# Initialize Nominatim API
+geolocator = Nominatim(user_agent="MyApp")
+
+#connect with mongo
 url = "mongodb+srv://ItsAtlant:irRNEj7rfzvzpWw7.@cluster0.gbqxuqm.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(url)
-
-print(client)
 mydb = client["ConcertoDB"]
-mycollection = mydb["Biglietti_venduti"]
+collection_biglietti = mydb["Biglietti_venduti"]
+collection_concerti = mydb["Concerto"]
 
-mydict = \
-    {
-        "id_concerto": ObjectId("648c38abab40123ad77944d2"),
-        "prezzo_pagato": 40,
-        "Anello/locazione": "prima fila",
-        "Nome e Cognome": "Davide Soltys",
-        "data acquisto": "22/04/2023"
-    }
+#login
+nickname = input("Ciao benvenuto nell'app dei concerti, perfavore inserisci il tuo nick name: ")
+citta = input("Inserisci la citta dove abiti: ")
+location = geolocator.geocode(citta)
+#scelta
+scelta = int(input("Per acquistare un biglietto premere 1, se vuoi visualizzare i tuoi biglietti premi 0: "))
+# visualizzare i biglietti
+if scelta == 0:
+    my_query = {"nome utente": nickname}
+    project = {"nome concerto": 1, "data concerto": 1, "prezzo pagato": 1, "data acquisto": 1}
 
-mycollection.insert_one(mydict)
-#collegamento
+    for x in collection_biglietti.find(my_query, project).sort("data concerto", 1):
+        print(x, "\n")
+
+
+if scelta == 1:
+    data = datetime.datetime.now()
+    print(data)
+    formatted_date = {"$date": data}
+    myquery = {"data": {"$gte": data}}
+    project = {"coordinate": 0}
+
+    for x in collection_concerti.find(myquery,project).sort("data", 1):
+        print(x, "\n")
+
+
+    print("Digitare R per cercare l'artista")
+    print("Digitare V per cercare per vicinanza")
+    print("Digitare S per cercare per costo")
+    print("Digitare N per cercare per nome")
+    print("Digitare A per cercare per artista")
+    print("Digitare Q per acquistare")
+    print("Digitare ESC per tornare nel menù")
+
+
